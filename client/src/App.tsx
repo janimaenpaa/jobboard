7c2e83d52de1bb0698be9c9c25a4f4f3849c5565
janimaenpaa <itsmayd@gmail.com>
2020-08-04 23:27:05 +0300

    Add Storybook to the project

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,7 +1,6 @@
 import React, { FC } from "react"
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom"
 import "typeface-roboto"
-import "typeface-open-sans"
 import "typeface-lato"
 import "typeface-roboto-slab"
 
---
+++ client/src/stories/index.stories.tsx
@@ -0,0 +1,7 @@
+import React from "react"
+
+import { Welcome } from "@storybook/react/demo"
+
+export default { title: "Welcome" }
+
+export const withText = () => <Welcome />
