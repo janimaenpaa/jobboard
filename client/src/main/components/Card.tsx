248780a7d5caa4f67e1b28a2fcfd3beb371d90c1
janimaenpaa <itsmayd@gmail.com>
2020-08-06 15:42:36 +0300

    Update component and story

--- client/src/main/components/Card.tsx
+++ client/src/main/components/Card.tsx
@@ -2,10 +2,6 @@ import React from "react"
 import styled from "styled-components"
 import { device } from "../theme"
 
-export interface Props {
-  children: React.ReactNode
-}
-
 const Container = styled.div`
   display: flex;
   flex-direction: column;
@@ -14,6 +10,55 @@ const Container = styled.div`
   border-radius: 10px;
   width: 60%;
   padding: 10px;
+  margin-top: 0.7rem;
+
+  @media ${device.laptop} {
+    width: 80%;
+  }
+
+  @media ${device.tablet} {
+    width: 100%;
+    border-radius: 0px;
+  }
+`
+
+const PinContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  flex-wrap: wrap;
+  background-color: #fff455;
+  border-radius: 10px;
+  width: 60%;
+  padding: 10px;
+  margin-top: 0.7rem;
+
+  &:hover {
+    background-color: #ffd302;
+  }
+
+  @media ${device.laptop} {
+    width: 80%;
+  }
+
+  @media ${device.tablet} {
+    width: 100%;
+    border-radius: 0px;
+  }
+`
+
+const HoverContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  flex-wrap: wrap;
+  background-color: white;
+  border-radius: 10px;
+  width: 60%;
+  padding: 10px;
+  margin-top: 0.7rem;
+
+  &:hover {
+    background-color: #81d2ff;
+  }
 
   @media ${device.laptop} {
     width: 80%;
@@ -29,7 +74,28 @@ const Content = styled.div`
   margin: 20px;
 `
 
-const Card: React.FC<Props> = ({ children }) => {
+export interface Props {
+  children: React.ReactNode
+  type?: string
+}
+
+const Card: React.FC<Props> = ({ type, children }) => {
+  if (type === "PIN") {
+    return (
+      <PinContainer>
+        <Content>{children}</Content>
+      </PinContainer>
+    )
+  }
+
+  if (type === "HOVER") {
+    return (
+      <HoverContainer>
+        <Content>{children}</Content>
+      </HoverContainer>
+    )
+  }
+
   return (
     <Container>
       <Content>{children}</Content>
--- client/src/stories/Card.stories.tsx
+++ client/src/stories/Card.stories.tsx
@@ -5,7 +5,6 @@ import Card from "../main/components/Card"
 export default {
   component: Card,
   title: "Card",
-  description: "It's a card",
 }
 
 export const card = () => (
@@ -22,3 +21,33 @@ export const card = () => (
     </p>
   </Card>
 )
+
+export const HoverableCard = () => (
+  <Card type="HOVER">
+    <h2 style={{ margin: 0 }}>Hello World!</h2>
+    <p>
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+      tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
+      veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
+      commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
+      velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat
+      cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id
+      est laborum.
+    </p>
+  </Card>
+)
+
+export const PinnedCard = () => (
+         <Card type="PIN">
+           <h2 style={{ margin: 0 }}>Hello World!</h2>
+           <p>
+             Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+             eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
+             ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+             aliquip ex ea commodo consequat. Duis aute irure dolor in
+             reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+             pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+             culpa qui officia deserunt mollit anim id est laborum.
+           </p>
+         </Card>
+       )
